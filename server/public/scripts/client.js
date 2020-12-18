7472645a0c46d83156658a12df5719ffd78688db
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2020-12-18 16:56:07 -0600

    targets operater buttons. adds event listener to push operator value to equation object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,5 +5,47 @@ $(document).ready(handleReady)
 function handleReady() {
     console.log('jq linked')
 
+    //event listeners
+    $('#add-button').on('click', addOperator)
+    $('#subtract-button').on('click', subtractOperator)
+    $('#multiply-button').on('click', multiplyOperator)
+    $('#divide-button').on('click', divideOperator)
+
+    $('#evaluate').on('click', calculate)
+    // $('#clear-button').on('click', clearHistory)
+    // renderToDom()
+
+}
+
+let equation = {}
+
+
+function calculate() {
+    console.log('in calculate')
+    equation.numOne = $('#numberOneIn').val()
+    equation.numTwo = $('#numberTwoIn').val()
+    console.log(equation)
+
     
 }
+
+function addOperator() {
+    equation.operator = '+'
+}
+
+function subtractOperator() {
+    equation.operator = "-"
+}
+
+function multiplyOperator() {
+    equation.operator = "*"
+}
+
+function divideOperator() {
+    equation.operator = "/"
+}
+
+
+
+
+
