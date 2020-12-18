ac980c9689b6eb16feb21b666b87aae86cfe8e87
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2020-12-18 15:13:14 -0600

    requires express and bodyparser on server. server running on port 5000. add inputs and buttons to index.html

---
+++ server/public/scripts/client.js
@@ -0,0 +1,9 @@
+console.log('js linked')
+
+$(document).ready(handleReady)
+
+function handleReady() {
+    console.log('jq linked')
+
+    
+}
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 5000
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('server/public'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('server is running on port', PORT)
+})
