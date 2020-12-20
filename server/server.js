9bb5e7c8544193acff1a18a6752d630edd83f0df
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2020-12-20 12:52:26 -0600

    adds comments to server.js and adds bootstrap container to calculator

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//set up for express server
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -7,10 +8,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('server/public'))
 
-
+// global array to collect calculate equation object history
 let history = []
 
-
+//function takes client side equation object and calculate the result. Pushes current answer into equation object, then pushes equation object to history array.
 function calculate(equation) {
     let numOne = Number(equation.numOne) 
     let numTwo = Number(equation.numTwo)
@@ -29,8 +30,7 @@ function calculate(equation) {
     history.push(equation)
 }
 
-
-
+// post route receives equation object, calls calculation function and responds with 201 status
 app.post('/calculate', (req, res) => {
     console.log('in POST /calculate')
     let equation = req.body
@@ -40,11 +40,13 @@ app.post('/calculate', (req, res) => {
     res.sendStatus(201)
 })
 
+// get route sends history array back to client side
 app.get('/calculate', (req, res) => {
     console.log('in GET /calculate')
     res.send(history)
 })
 
+// delete route sets history variable to empty array. sends empty history array back to client side.
 app.delete('/history', (req, res) => {
     console.log('in DELETE /history')
     history = []
